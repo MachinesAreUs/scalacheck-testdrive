fd8f331dd418a4e6004d34d2c13dd5cdf22716d2
Agustin Ramos Fonseca <zentimental.software@gmail.com>
2015-06-08 19:48:21 -0500

    Sudoku specs. Failing to run.

--- src/test/scala/net/zensoft/games/sudoku/SudokuSpec.scala
+++ src/test/scala/net/zensoft/games/sudoku/SudokuSpec.scala
@@ -1,11 +1,22 @@
+import net.zensoft.games.sudoku.Sudoku
 import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
+import org.scalacheck.Gen
 
 object SudokuSpecification extends Properties("Sudoku") {
 
-  property("startsWith") = forAll { (a: String, b: String) =>
-    (a+b).startsWith(a)
+  property("all rows have the 9 digits") = forAll { (problem: Array[Array[Integer]]) =>
+    val sudoku = new Sudoku(problem)
+    sudoku != null
   }
+
+  property("all columns have the 9 digits") = forAll { (problem: Array[Array[Integer]]) =>
+    problem.sortWith(_.size < _.size).size == problem.size
+  }
+  
+  //implicit def intToInteger(i: Int) = i:java.lang.Integer
+  
+  def getInteger: Gen[Integer] = Gen.choose(1:java.lang.Integer, 9:java.lang.Integer)
 
 }
 
